03fd8a3180239af59f382e468accf4c5fce8ae4e
saurabh-jadhav-09 <94519671+saurabh-jadhav-09@users.noreply.github.com>
2025-01-07 15:20:35 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull saurabh9jadhav/python-sample-001:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 saurabh9jadhav/python-sample-001:latest
